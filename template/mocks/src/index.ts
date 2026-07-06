3c839394f5bbdc35da385642df47f32cafb4b5e4
Simon Bächler <b@chler.com>
2026-07-06 22:31:29 +0200

    update dependencies

--- template/mocks/src/index.ts
+++ template/mocks/src/index.ts
@@ -1,4 +1,4 @@
-export { datasetConfigs } from '#/generators/config.js';
-export type { DatasetConfig, DatasetPreset } from '#/generators/config.js';
-export { buildDataset } from '#/generators/dataset.js';
-export type { Dataset, DatasetIndexes } from '#/generators/dataset.js';
+export { datasetConfigs } from './generators/config.js';
+export type { DatasetConfig, DatasetPreset } from './generators/config.js';
+export { buildDataset } from './generators/dataset.js';
+export type { Dataset, DatasetIndexes } from './generators/dataset.js';
